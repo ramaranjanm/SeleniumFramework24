3b54dae5ad035e806711513b9d88a4715b6e833c
ramaranjanm <mishraramaranjan1@gmail.com>
2024-04-28 22:47:52 +0530

    push code to test webhook scenario

---
+++ src/test/java/com/qa/opencart/tests/TestWebhookGitJenkin.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.tests;
+
+public class TestWebhookGitJenkin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubsys
+		
+		System.out.println("test Webbook");
+	}
+
+}
